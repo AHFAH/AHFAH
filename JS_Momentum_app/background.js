767fdf83196b6d560a180a4646dd08dd04c28e65
AHFAH <95862526+AHFAH@users.noreply.github.com>
2022-01-26 23:38:57 +0900

    Update background.js

--- JS_Momentum_app/background.js
+++ JS_Momentum_app/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg"];
 
 const chImage = images[Math.floor(Math.random() * images.length)];
 const wrap = document.querySelector("div #wrap-img");
